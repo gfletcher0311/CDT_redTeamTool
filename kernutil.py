f3b13e173489ad7d5ce24e0ac80deacd65a990ce
Gavin <gfletch0311@gmail.com>
2024-10-18 13:31:01 -0400

    Changed keyboard idea. Testing time.

--- kernutil.py
+++ kernutil.py
@@ -1,13 +1,54 @@
 import subprocess
 import time
+import sys
+import threading
+message = "You cannot type right now, you are resting."
+message_index = 0
 
-def disableKeyboard():
-    #disable their keyboard for 30ish seconds
-    result = subprocess.run("xinput list | grep -i 'keyboard' | grep -o '[0-9]*'", shell=True, capture_output=True, text=True)
-    keyboard_id = result.stdout.splitlines()[0] # The master keyboard will be the first number from result
-    disable_cmd = subprocess.run(["xinput", "disable", keyboard_id])
-    time.wait(5)
-    enable_cmd = subprocess.run(["xinput", "enable", keyboard_id])
+
+
+def install_pynput():
+    try:
+        import pynput  
+    except ModuleNotFoundError:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "pynput"])
+        
+install_pynput()        
+from pynput import keyboard # We can now import pynput
+from pynput.keyboard import Controller
+
+
+def open_terminal():
+    subprocess.run("x-temrinal-emulator")
+
+def typing_replacement():
+    open_terminal() # opens a terminal for typing
+    time.sleep(1)
+    block_keyboard = True
+    #disable their keyboard for 30ish seconds (will only type "You cannot type you are resting" no matter what keys are pressed)
+    keyboard = Controller()
+    if not block_keyboard: #if block_keyboard = false
+        return
+    with keyboard.Listener(on_press=on_press) as listener:
+        def timer():
+            time.sleep(10) # CHANGE THIS FOR DURATION
+            block_keyboard = False
+            listener.stop()
+            
+        countown_thread = threading.Thread(target=timer)
+        countown_thread.start()
+        listener.join()
+        
+	
+    
+    
+def on_press():
+    current_char = message[message_index]
+    message_index = (message_index + 1) % len(message)
+    keyboard.type(current_char)
+
+
+
 def grass():
     ascii_grass= """
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -36,8 +77,7 @@ def alert():
 
 
 def main():
-	disableKeyboard()
-	
+    typing_replacement()
 	
 
 
