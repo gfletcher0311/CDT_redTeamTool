f7ab7532c16ae5c05b6661f24daf7d802ef8cdc8
Gavin Fletcher <gfletch0311@gmail.com>
2024-10-18 22:39:51 -0400

    testing fix

--- grass.py
+++ grass.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python
 import subprocess
 import time
 import threading
+import random
+
 def install_pynput():
     try:
         import pynput  
@@ -13,9 +16,16 @@ from pynput.keyboard import Key, Controller
 from pynput import keyboard
 
 def open_terminal_with_text(text):
+    screen_width = 1920
+    screen_height = 1080
+
+    # Generate random coordinates for the terminal position
+    random_x = random.randint(0, screen_width - 400)
+    random_y = random.randint(0, screen_height - 300) 
+    geometry = f"450x300+{random_x}+{random_y}"
     # Using a shell command that maintains the terminal open
-    command = f'bash -c "echo \'{text}\'; read -p \'keep touching grass\'"'
-    subprocess.Popen(['x-terminal-emulator', '-e', command])
+    command = f'bash -c "echo \'{text}\'; read -p \'keep calm, touch some grass :)\'"'
+    subprocess.Popen(['x-terminal-emulator', '-geometry', geometry, '-e', command])
 
 
 def grass():
@@ -51,8 +61,11 @@ def alert():
 def on_press(key):
     if key == keyboard.Key.enter:
         grass()
+        grass()
 def main():
     with keyboard.Listener(on_press=on_press) as listener:
         listener.join()  # Keep the listener running
     grass()
     
+if __name__ == "__main__":
+    main()
